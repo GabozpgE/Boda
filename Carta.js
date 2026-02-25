de9efb143ba3ae9f48801efb9984d2e1ff030d33
GabozpgE <gabriel.zamorap@uanl.edu.mx>
2026-02-24 20:07:23 -0600

    cambie el index
    cambie el index por Carta y viseversa

--- Carta.js
+++ Carta.js
@@ -4,5 +4,5 @@ const preload = (srcs=[]) => srcs.forEach(s => { const i=new Image(); i.src=s; }
 
 document.getElementById("openBtn").addEventListener("click", () => {
   // Cambia el nombre por tu segunda ventana
-  window.location.href = "Index.html";
+  window.location.href = "Carta.html";
 });
